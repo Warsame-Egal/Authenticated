07a0ae3a6ad19162882bf907d996fbd1952026aa
Warsame Egal <warsameegal0@gmail.com>
2025-02-23 20:28:40 -0500

    Add and test basic AdminController and UserController

---
+++ src/main/java/com/warsameegal/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package com.warsameegal.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController // Make it a component of spring
+@RequestMapping("/admin") // map to slash admin
+@CrossOrigin("*") // make request across different origins
+public class AdminController {
+
+    @GetMapping("/")
+    public String helloAdminController() {
+        return "Admin level access";
+    }
+
+    // Ensure there are no invalid characters in method names or request mappings
+}
--- src/main/java/com/warsameegal/controllers/UserController.java
+++ src/main/java/com/warsameegal/controllers/UserController.java
@@ -1,14 +1,16 @@
-package com.warsameegal.Controllers;
+package com.warsameegal.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequestMapping("/user")
-@CrossOrigin("*")
+@RestController // Make it a component of spring
+@RequestMapping("/user") // map to slash admin
+@CrossOrigin("*") // make request across different origins
 public class UserController {
 
+    @GetMapping("/")
     public String helloUserController() {
         return "User access level";
     }
